fb5e2fdd5a24a9151476a6b001869b52b928ba06
huytv <huytv593@gmail.com>
2016-09-18 22:39:58 +0700

    add libogg lib

--- recipes/source.rb
+++ recipes/source.rb
@@ -41,6 +41,20 @@ bash "install_x265" do
 	EOH
 end
 
+# Install libogg
+bash "install_libvorbis" do
+	cwd "#{node[:ffmpeg][:source_path]}"
+	code <<-EOH
+	curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+	tar xzvf libogg-1.3.2.tar.gz
+	cd libogg-1.3.2
+	./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+	make
+	make install
+	make distclean
+	EOH
+end
+
 # Install libfdk_aac
 bash "install_libfdk_aac" do
 	cwd "#{node[:ffmpeg][:source_path]}"
@@ -86,8 +100,8 @@ bash "install_libfdk_aac" do
 	EOH
 end
 
-# Install libogg
-bash "install_libogg" do
+# Install libvorbis
+bash "install_libvorbis" do
 	cwd "#{node[:ffmpeg][:source_path]}"
 	code <<-EOH
 	curl -L -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
